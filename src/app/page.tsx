18c85ccdf20d7dac263d9ef5cb5d5089e284980a
Krzysztof <kgruca21@gmail.com>
2023-05-10 20:21:03 -0700

    minor changes to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ export default function Home() {
   return (
     <div
       className="h-screen flex flex-col bg-gradient-to-b from-cyan-800 via-blue-950 
-      to-indigo-950 justify-center items-center"
+      to-indigo-950 items-center pt-32"
     >
-      <div>
-        <h1 className="text-5xl">Welcome to Overwatching</h1>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-5xl">Guess My SR</h1>
+        <div className="flex flex-row justify-center items-center">
+          <p className="mr-3">Sign up for the immersive experience</p> |
+          <p className="ml-3"> Already have an account? Log in</p>
+        </div>
       </div>
     </div>
   );
